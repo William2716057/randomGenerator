161e673898202db0d49c9ac493daccbbb3fe6b39
William <willspri@gmail.com>
2024-07-08 17:39:23 +1200

    email added

--- RandomGenerator.py
+++ RandomGenerator.py
@@ -1,4 +1,3 @@
-import base64
 import random
 import string
 
@@ -7,7 +6,15 @@ def generate_random_string(length=8):
     random_string = ''.join(random.choice(characters) for i in range(length))
     return random_string
 
-print(generate_random_string())
+# Generate random string
+random_string = generate_random_string()
+print(random_string)
 
-sample_bytes = generate_random_string().encode("ascii")
+# Encode string to bytes and then back to string to avoid b'...'
+sample_bytes = random_string.encode("ascii")
 print(sample_bytes)
+decoded_string = sample_bytes.decode("ascii")
+print(decoded_string)
+
+# Print result as an email address
+print(f"{decoded_string}@outlook.com")
