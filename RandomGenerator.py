2022df5c29a2ecb44d72935e1a374ea5bf7bfd35
William <willspri@gmail.com>
2024-07-08 17:30:43 +1200

    committed

---
+++ RandomGenerator.py
@@ -0,0 +1,10 @@
+import base64
+import random
+import string
+
+def generate_random_string(length=8):
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(characters) for i in range(length))
+    return random_string
+
+print(generate_random_string())
